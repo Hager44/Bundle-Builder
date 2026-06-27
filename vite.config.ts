e29dd156f82612c3edbc94d6396b29e1f31263d7
Hager44 <hager384@gmail.com>
2026-06-27 15:47:09 +0300

    adding env variable of Vite for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/Bundle-Builder",
   resolve: {
     alias: {
       "@app": path.resolve(__dirname, "src"),
